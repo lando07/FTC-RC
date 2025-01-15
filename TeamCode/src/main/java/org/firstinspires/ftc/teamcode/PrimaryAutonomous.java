414a4c20330baf1e5014ecae8d2ad681c1f1f218
lando07 <96402450+lando07@users.noreply.github.com>
2025-01-15 16:20:50 -0600

    added telem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrimaryAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrimaryAutonomous.java
@@ -19,9 +19,9 @@ public class PrimaryAutonomous extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, startingPose);
         RaiseArmSlider slider = new RaiseArmSlider(this, "raiseArmSlider");
         Claw primaryClaw = new Claw(this, "primaryClaw");
-        hardwareMap.get(DcMotor.class, "armSlider").setPower(0.5);
-        sleep(1200);
-        hardwareMap.get(DcMotor.class, "armSlider").setPower(0);
+//        hardwareMap.get(DcMotor.class, "armSlider").setPower(0.5);
+//        sleep(1200);
+//        hardwareMap.get(DcMotor.class, "armSlider").setPower(0);
 
         Pose2d pose1 = new Pose2d(0, 33, Math.toRadians(270));
         Pose2d pose2 = new Pose2d(3, 33, Math.toRadians(270));
@@ -38,16 +38,19 @@ public class PrimaryAutonomous extends LinearOpMode {
         waitForStart();
         if (opModeIsActive()) {
             //Clip first specimen
+
             slider.doHighSpecimenLowBasket();
             Actions.runBlocking(drive.actionBuilder(startingPose)
                     .splineToConstantHeading(new Vector2d(0, 33), Math.toRadians(270))
                     .build());
             //release first specimen
+            telemetry.addLine("Clipped First Specimen");
             primaryClaw.openClaw();
             sleep(50);
             Actions.runBlocking(drive.actionBuilder(pose1)
                     .strafeTo(new Vector2d(0, 36))
                     .build());
+            telemetry.addLine("Going to 2nd specimen");
             //strafe to second specimen on the field
             slider.resetHeight();
             Actions.runBlocking(drive.actionBuilder(pose2)
@@ -56,13 +59,13 @@ public class PrimaryAutonomous extends LinearOpMode {
                     .strafeTo(new Vector2d(-48, 13))
                     //Push second into observation zone
                     .strafeTo(new Vector2d(-48, 52))
-
                     .turnTo(Math.toRadians(90))
                     .build());
             //grab third specimen
             primaryClaw.openClaw();
             sleep(400);
             primaryClaw.closeClaw();
+            telemetry.addLine("Grabbing third specimen");
             slider.doHighSpecimenLowBasket();
             Actions.runBlocking(drive.actionBuilder(pose3)
                     .strafeToLinearHeading(new Vector2d(3, 36), Math.toRadians(270))
@@ -71,6 +74,7 @@ public class PrimaryAutonomous extends LinearOpMode {
             //release clipped third specimen, and RTB
             sleep(50);
             primaryClaw.openClaw();
+            telemetry.addLine("Clipped third specimen");
             Actions.runBlocking(drive.actionBuilder(pose4)
                     .strafeTo(new Vector2d(3, 36))
                     .build());
@@ -78,7 +82,7 @@ public class PrimaryAutonomous extends LinearOpMode {
             Actions.runBlocking(drive.actionBuilder(pose5)
                     .strafeToLinearHeading(new Vector2d(-48, 52), Math.toRadians(90))
                     .build());
-
+            telemetry.addLine("Grabbed 2nd specimen");
             //Grab second specimen and clip
             primaryClaw.openClaw();
             sleep(400);
@@ -90,14 +94,17 @@ public class PrimaryAutonomous extends LinearOpMode {
                     .build());
             primaryClaw.openClaw();
             sleep(50);
+            telemetry.addLine("Clipped second specimen");
             Actions.runBlocking(drive.actionBuilder(pose7)
                     .strafeTo(new Vector2d(-3, 36))
                     .build());
+            telemetry.addLine("RTB");
             //Park in observation zone
             slider.resetHeight();
             Actions.runBlocking(drive.actionBuilder(pose8)
                     .strafeTo(new Vector2d(-48, 54))
                     .build());
+            telemetry.addLine("DONE");
         }
     }
 }
