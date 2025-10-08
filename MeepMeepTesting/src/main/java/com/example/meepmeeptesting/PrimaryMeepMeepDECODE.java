39116ccc8be298e6aa6fc2802f2eda69265a3a3b
Landon Smith <218216790+kaedontt11@users.noreply.github.com>
2025-10-08 17:03:38 -0500

    Update PrimaryMeepMeepDECODE.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PrimaryMeepMeepDECODE.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PrimaryMeepMeepDECODE.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class PrimaryMeepMeepDECODE {
-    public static Pose2d startingPose = new Pose2d(0,0,0);
+    public static Pose2d startingPose = new Pose2d(-50,-50,Math.toRadians(45));
 
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "true");
@@ -17,8 +17,8 @@ public class PrimaryMeepMeepDECODE {
                 .setConstraints(90, 70, 55, 60, 14)
                 .build();
         myBot.runAction(myBot.getDrive().actionBuilder(startingPose)
-                        .strafeToConstantHeading(new Vector2d(15,15))
-                        .strafeToConstantHeading(new Vector2d(-15,30))
+
+                        .strafeToConstantHeading(new Vector2d(-52,-16))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
