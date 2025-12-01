3776443b8e8ec65f8de5017b747c9b502e4fe61c
Landon Smith <218216790+kaedontt11@users.noreply.github.com>
2025-11-30 19:49:57 -0600

    Update

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DECODE/RedLaunchZoneDECODE.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DECODE/RedLaunchZoneDECODE.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class RedLaunchZoneDECODE {
-    public static Pose2d startingPose = new Pose2d(-53.1,46.1,Math.toRadians(-52));
+    public static Pose2d startingPose = new Pose2d(-53.1,46.1,Math.toRadians(-232));
 //TODO : fix rotation
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "true");
