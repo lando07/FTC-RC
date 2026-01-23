b11c5e4b4b6b77d1a34c5512752e450d5c0cc282
land07 <96402540+lando07@users.noreply.github.com>
2026-01-22 23:24:10 -0600

    Updated SDK to v11.1, and Started work on improving input latency and motor write caching

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -32,6 +32,7 @@ public final class PinpointLocalizer implements Localizer {
         // TODO: make sure your config has a Pinpoint device with this name
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
         driver = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        driver.setErrorDetectionType(GoBildaPinpointDriver.ErrorDetectionType.CRC);
 
 
         double mmPerTick = inPerTick * 25.4;
